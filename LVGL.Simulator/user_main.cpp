c309eb4c88ba888f1ad470fc69802c227c772c15
浩然 张 <2578255853@qq.com>
2026-04-09 17:12:33 +0800

    创建对象、大小位置对齐设置测试

--- LVGL.Simulator/user_main.cpp
+++ LVGL.Simulator/user_main.cpp
@@ -4,9 +4,24 @@
 
 void user_main() {
 
-    lv_obj_t* switch_obj = lv_switch_create(lv_scr_act());
-    lv_obj_set_size(switch_obj, 120, 60);
-    lv_obj_align(switch_obj, LV_ALIGN_CENTER, 0, 0);
+    //创建对象
+    lv_obj_t* obj1 = lv_obj_create(lv_scr_act());//以屏幕作为父对象创建对象
+    lv_obj_t* obj2 = lv_obj_create(obj1);//以现有对象作为父对象创建对象
+    lv_obj_t* obj3 = lv_obj_create(obj1);
+    //大小设置
+    lv_obj_set_width(obj1, 400);//设置对象宽度
+    lv_obj_set_height(obj1, 300);//设置对象高度
+    lv_obj_set_size(obj2, 100, 100);//设置对象宽和高
+    lv_obj_set_size(obj3, 100, 100);
+    //位置设置
+    lv_obj_set_x(obj1, 50);//设置对象x坐标
+    lv_obj_set_y(obj1, 50);//设置对象y坐标
+    lv_obj_set_pos(obj1, 100, 100);//设置对象x+y坐标
+    //对齐设置
+    lv_obj_set_align(obj2, LV_ALIGN_CENTER);//以父对象中心对齐
+    lv_obj_set_align(obj2, LV_ALIGN_RIGHT_MID);//以父对象右侧对齐
 
-    int a = 1;
-}
+     
+
+    
+} 
